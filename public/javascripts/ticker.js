c094d8758e87a9d3fe830ee4ab41afad3047a18c
Ryan Platt - VG <ryanplatt-vg@Ryans-MacBook-Pro-2.local>
2017-07-27 10:38:12 -0400

    update mongo data

--- public/javascripts/ticker.js
+++ public/javascripts/ticker.js
@@ -10,11 +10,18 @@
         }).then(function successCallback(response) {
             $scope.vanguardTickerData = [];
             $scope.nonVanguardTickerData = [];
+            $scope.totalTickerAmount = 0;
             for (var i in response.data) {
                 if (response.data[i].tickerCompany === "Vanguard") {
-                    $scope.vanguardTickerData.push(response.data[i]);
+                    var newData = response.data[i];
+
+                    newData.tickerTotal = (newData.tickerPrice * 5).toFixed(2);
+                    $scope.vanguardTickerData.push(newData);
                 } else {
-                    $scope.nonVanguardTickerData.push(response.data[i]);
+                    var newData = response.data[i];
+
+                    newData.tickerTotal = (newData.tickerPrice * 5).toFixed(2);
+                    $scope.nonVanguardTickerData.push(newData);
                 }
             }
         }, function errorCallback(response) {
