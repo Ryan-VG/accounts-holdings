f0d2819bb7ddbd154c2dc442e49dea05c1a38be2
Ryan Platt - VG <ryanplatt-vg@Ryans-MacBook-Pro-2.local>
2017-07-28 07:48:40 -0400

    basic logging for api route

--- routes/api.js
+++ routes/api.js
@@ -13,6 +13,7 @@ mongoose.connect(connStr, { useMongoClient: true })
 
 router.get('/rando', function(req, res, next) {
     res.json(process.env.rando);
+    console.log("\"current_date\":\"" + Date.now() + "\"|" + "\"current_process\":\"" + process.env.rando + "\"");
 });
 
 router.get('/users', function(req, res, next) {
@@ -22,8 +23,10 @@ router.get('/users', function(req, res, next) {
         if (err) {
             throw err;
 		} else if (users) {
+            console.log("\"current_date\":\"" + Date.now() + "\"|" + "\"all_users\":\"" + users + "\"");
 			res.json(users);
 		} else {
+            console.log("\"current_date\":\"" + Date.now() + "\"|" + "\"all_users\":\"ERROR::Couldn't retrieve users\"");
 			res.json({
 				"error": "no users found"
 			});
@@ -38,8 +41,10 @@ router.get('/ticker', function(req, res, next) {
         if (err) {
             throw err;
 		} else if (tickers) {
+            console.log("\"current_date\":\"" + Date.now() + "\"|" + "\"all_tickers\":\"" + tickers + "\"");
 			res.json(tickers);
 		} else {
+            console.log("\"current_date\":\"" + Date.now() + "\"|" + "\"all_tickers\":\"ERROR::Couldn't retrieve tickers\"");
 			res.json({
 				"error": "no tickers found"
 			});
